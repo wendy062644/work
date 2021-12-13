bb1914828946d59d1d8bca0676423e23b9adbe4c
Leo <75311991+wendy062644@users.noreply.github.com>
2021-12-13 11:05:15 +0800

    Update work 20211213.cpp

--- work 20211213.cpp
+++ work 20211213.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 #include<windows.h>
-
+#include<string.h>
+#include<conio.h>
+#include<iomanip>
 using namespace std;
 void Set(int x, int y);
 int main(){
-	char p[3] = {'/', 'x', '-'}; 
+	char p[3] = {'/', 'x', '-'};
+	string print;
 	int n = 7, m = 0;
 	cout << " ---------------------" << endl;
 	for(int i = 0; 3 > i; i++)
@@ -50,7 +53,57 @@ int main(){
 	cout << " ---------------------" << endl;
 	while(1)
 	{
-		
+		if(_kbhit())
+		{
+			char num = getch();
+			switch(num){
+				case 48:
+					print = print + '0';
+					break;
+				case 49:
+					print = print + '1';
+					break;
+				case 50:
+					print = print + '2';
+					break;
+				case 51:
+					print = print + '3';
+					break;
+				case 52:
+					print = print + '4';
+					break;
+				case 53:
+					print = print + '5';
+					break;
+				case 54:
+					print = print + '6';
+					break;
+				case 55:
+					print = print + '7';
+					break;
+				case 56:
+					print = print + '8';
+					break;
+				case 57:
+					print = print + '9';
+					break;
+				case 47:
+					print = print + '/';
+					break;
+				case 43:
+					print = print + '+';
+					break;
+				case 42:
+					print = print + 'x';
+					break;
+				case 45:
+					print = print + '-';
+					break;
+			}
+			Set(4, 2);
+			cout << setw(15) << print << endl;
+		}
+		Sleep(100);
 	}
 }
 void Set(int x, int y){
